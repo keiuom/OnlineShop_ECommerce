679eab2ef234e9b57996e62e3f7b9b44b029722a
Md Keiuom <keiuom.miah@wundermanthompson.com>
2023-04-14 17:11:43 +0600

    Service class library added for Order module

---
+++ Order.Services/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Order.Services
+{
+    public static class DependencyInjection
+    {
+        public static void LoadDependency(this IServiceCollection services)
+        {
+
+        }
+    }
+}
