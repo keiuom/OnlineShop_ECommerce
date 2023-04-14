bf00dd8ebfd1d97f569143a8309743ee4024faca
Md Keiuom <keiuom.miah@wundermanthompson.com>
2023-04-14 11:37:07 +0600

    BaseEntity added in Core layer

---
+++ BuyNow.Core/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuyNow.Core
+{
+    public abstract class BaseEntity<T>
+    {
+        [Key]
+        public T Id { get; set; } = default!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
