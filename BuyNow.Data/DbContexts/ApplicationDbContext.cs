87e6b9a570f4ae69b4b1363f16c4fab6048e25c5
Md Keiuom <keiuom.miah@wundermanthompson.com>
2023-04-14 11:30:22 +0600

    ApplicationDbContext and DependencyInjection added

---
+++ BuyNow.Data/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BuyNow.Data.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            base.OnConfiguring(dbContextOptionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
