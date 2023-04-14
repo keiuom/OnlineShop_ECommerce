f7082739179f373eda21eb5cf40af6837643d3a5
Md Keiuom <keiuom.miah@wundermanthompson.com>
2023-04-14 15:26:20 +0600

    Products controller added in Inventory module in API project

---
+++ BuyNow.API/Inventory/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using BuyNow.Core.Common;
+using Inventory.Common.DTOs;
+using Inventory.Common.Models;
+using Inventory.Services.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuyNow.API.Inventory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GetProductDto>> AddProductAsync(AddProductModel productModel)
+        {
+            return await _productService.AddProductAsync(productModel);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<GetProductDto>> EditProductAsync(EditProductModel productModel)
+        {
+            return await _productService.EditProductAsync(productModel);
+        }
+
+        [HttpDelete("id")]
+        public async Task<ActionResult<Response>> DeleteProductAsync(int id)
+        {
+            return await _productService.DeleteProductAsync(id);
+        }
+    }
+}
