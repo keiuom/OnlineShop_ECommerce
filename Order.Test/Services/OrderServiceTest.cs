0ce816c093d38214d03e014afa44a195fe7fb741
Md Keiuom <keiuom.miah@wundermanthompson.com>
2023-04-28 16:03:19 +0600

    possible test cases added for GetOrderAsync method in OrderService

--- Order.Test/Services/OrderServiceTest.cs
+++ Order.Test/Services/OrderServiceTest.cs
@@ -1,5 +1,8 @@
 ﻿using Autofac.Extras.Moq;
+using BuyNow.Core.Common;
+using BuyNow.Core.Helpers;
 using Moq;
+using Order.Common.DTOs;
 using Order.Common.Models;
 using Order.Services.HttpClients;
 using Order.Services.Mails;
@@ -21,6 +24,11 @@ namespace Order.Test.Services
         private Mock<IEmailMessageService> _emailMessageServiceMock;
         private Mock<IOrderClient> _orderClientMock;
         private IOrderService _orderService;
+
+        private const int _pageNumber = 1;
+        private const int _pageSize = 10;
+        private const int _totalCount = 10;
+        private const string _customerEmail = "user1@gmail.com";
 
         [SetUp]
         public void Setup()
@@ -153,6 +161,127 @@ namespace Order.Test.Services
             _repositoryMock.Verify(r => r.SaveAsync(), Times.Once);
         }
 
+        [Test]
+        public async Task GetOrdersAsync_ReturnsNonNullResult()
+        {
+            // Arrange
+            var orders = new PagedList<OrderEO>(new List<OrderEO>(), _totalCount, _pageNumber, _pageSize);
+
+            _repositoryMock.Setup(r => r.OrderRepository)
+                .Returns(_orderRepositoryMock.Object);
+
+            _repositoryMock.Setup(r => r.OrderRepository.GetOrdersAsync(_pageNumber, _pageSize))
+                .ReturnsAsync(orders)
+                .Verifiable();
+
+            // Act
+            var result = await _orderService.GetOrdersAsync(_pageNumber, _pageSize);
+
+            // Assert
+            result.ShouldSatisfyAllConditions(
+                result => result.ShouldNotBeNull(),
+                result => _repositoryMock.Verify()
+                );
+        }
+
+        [Test]
+        public async Task GetOrdersAsync_ReturnsOrderListDto()
+        {
+            // Arrange
+            var orders = new PagedList<OrderEO>(new List<OrderEO>(), _totalCount, _pageNumber, _pageSize);
+
+            _repositoryMock.Setup(r => r.OrderRepository)
+                .Returns(_orderRepositoryMock.Object);
+
+            _repositoryMock.Setup(r => r.OrderRepository.GetOrdersAsync(_pageNumber, _pageSize))
+                .ReturnsAsync(orders)
+                .Verifiable();
+
+            // Act
+            var result = await _orderService.GetOrdersAsync(_pageNumber, _pageSize);
+
+            // Assert
+            result.ShouldSatisfyAllConditions(
+                result => result.ShouldBeOfType<OrderListDto>(),
+                result => _repositoryMock.Verify()
+                );
+        }
+
+        [Test]
+        public async Task GetOrdersAsync_ReturnsCorrectNumberOfOrders()
+        {
+            // Arrange
+            var orders = GetOrderEntityList();
+
+            _repositoryMock.Setup(r => r.OrderRepository)
+                .Returns(_orderRepositoryMock.Object);
+
+            _repositoryMock.Setup(x => x.OrderRepository.GetOrdersAsync(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(new PagedList<OrderEO>(orders, _totalCount, _pageNumber, _pageSize))
+                .Verifiable();
+
+            // Act
+            var result = await _orderService.GetOrdersAsync(_pageNumber, _pageSize);
+
+            // Assert
+            result.ShouldSatisfyAllConditions(
+                result => result.Orders.Count.ShouldBeEquivalentTo(orders.Count),
+                result => _repositoryMock.Verify()
+                );
+        }
+
+        [Test]
+        public async Task GetOrdersAsync_ReturnsCorrectPagingInformation()
+        {
+            // Arrange
+            var currentPage = 1;
+            var pageSize = 10;
+            var totalItems = 10;
+            var orders = GetOrderEntityList();
+
+            _repositoryMock.Setup(r => r.OrderRepository)
+                .Returns(_orderRepositoryMock.Object);
+
+            _repositoryMock.Setup(x => x.OrderRepository.GetOrdersAsync(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(new PagedList<OrderEO>(orders, _totalCount, _pageNumber, _pageSize))
+                .Verifiable();
+
+            // Act
+            var result = await _orderService.GetOrdersAsync(_pageNumber, _pageSize);
+
+            // Assert
+            result.ShouldSatisfyAllConditions(
+                result => result.Paging.CurrentPage.ShouldBeEquivalentTo(currentPage),
+                result => result.Paging.ItemsPerPage.ShouldBeEquivalentTo(pageSize),
+                result => result.Paging.TotalItems.ShouldBeEquivalentTo(totalItems),
+                result => _repositoryMock.Verify()
+                );
+        }
+
+        [Test]
+        public async Task GetOrdersAsync_ReturnsCorrectOrderInformation()
+        {
+            // Arrange
+            var orders = GetOrderEntityList();
+
+            _repositoryMock.Setup(r => r.OrderRepository)
+                .Returns(_orderRepositoryMock.Object);
+
+            _repositoryMock.Setup(x => x.OrderRepository.GetOrdersAsync(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(new PagedList<OrderEO>(orders, _totalCount, _pageNumber, _pageSize));
+
+            // Act
+            var result = await _orderService.GetOrdersAsync(_pageNumber, _pageSize);
+
+            // Assert
+            result.ShouldSatisfyAllConditions(
+                result => result.Orders[0].Id.ShouldBeEquivalentTo(orders[0].Id),
+                result => result.Orders[0].CustomerEmail.ShouldBeEquivalentTo(orders[0].CustomerEmail),
+                result => result.Orders[1].Id.ShouldBeEquivalentTo(orders[1].Id),
+                result => _repositoryMock.Verify()
+                );
+        }
+
         private OrderModel GetOrderModel()
         {
             return new OrderModel
@@ -162,6 +291,16 @@ namespace Order.Test.Services
                     new ProductModel { Id = 1, Quantity = 2 },
                     new ProductModel { Id = 2, Quantity = 1 },
                 },
+            };
+        }
+
+        private List<OrderEO> GetOrderEntityList()
+        {
+           return new List<OrderEO>
+            {
+                new OrderEO { Id = 1, CustomerEmail = _customerEmail },
+                new OrderEO { Id = 2, CustomerEmail = _customerEmail },
+                new OrderEO { Id = 3, CustomerEmail = _customerEmail },
             };
         }
 
